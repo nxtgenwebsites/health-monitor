c942229c69a71e425fc324ffa901a9614da68cca
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-04-28 10:34:03 +0500

    labels

--- user-dashboard/js/temperature.js
+++ user-dashboard/js/temperature.js
@@ -8,7 +8,7 @@ function initializeTemperatureChart() {
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['0°', '10°', '20°', '30°', '40°', '50°', '60°', '70°', '80°', '90°', '100°'],
+            labels: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
             datasets: [
                 {
                     label: 'Celsius (°C)',
